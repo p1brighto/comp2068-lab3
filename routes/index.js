5f847d48d277a48c9070d41e3f92a5d62d0f7152
p1brighto <p1brighto@gmail.com>
2016-02-04 15:54:31 -0500

    added contact an other pages

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,13 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express on Azure' });
 });
+/* GET About page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About Me' });
+});
+/* GET Contact page. */
+router.get('/contact', function(req, res, next) {
+  res.render('contact', { title: 'Contact Page' });
+});
 
 module.exports = router;
